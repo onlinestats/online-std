53fa91b971f56b290871e62c8c5f61c949cd5d61
zemlyansky <anton.zemlyansky@gmail.com>
2018-10-15 10:34:04 +0300

    Add function name and getters

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const Variance = require('online-variance')
+var Variance = require('online-variance')
 
 module.exports = function (params) {
-  const v = Variance(params)
-  return function (x) {
-    return Math.sqrt(v(x))
+  var v = Variance(params)
+  var n = 0
+  var value
+
+  var std = function std (x, seriesObject) {
+    if (seriesObject && seriesObject.hasOwnProperty('variance') && !isNaN(seriesObject['variance'])) {
+      value = Math.sqrt(seriesObject.variance)
+      n = seriesObject.n
+    } else {
+      value = Math.sqrt(v(x))
+      console.log(v.n)
+      n = v.n
+    }
+    return value
   }
+
+  std.fit = function (x, seriesObject) {
+    std(x, seriesObject)
+  }
+
+  Object.defineProperty(std, 'value', {
+    get: function () {
+      return value
+    }
+  })
+
+  Object.defineProperty(std, 'n', {
+    get: function () {
+      return n
+    }
+  })
+
+  return std
 }
--- test.js
+++ test.js
@@ -6,15 +6,15 @@ const values = [1, 3, 5, NaN, 'Bob', '6', 8]
 test('Population standard deviation: NaN and Strings', (_) => {
   var std = StandardDeviation()
   values.forEach(v => { std(v) })
-  console.log(std())
   _.true(std() - 2.416609194 < 0.001)
+  _.true(std.value - 2.416609194 < 0.001)
+  _.equal(std.n, 5)
   _.end()
 })
 
-test('Sample variance: NaN and Strings', (_) => {
+test('Sample standard deviation: NaN and Strings', (_) => {
   var std = StandardDeviation({ddof: 1})
-  values.forEach(v => { std(v) })
-  console.log(std())
-  _.true(std() - 2.701851217 < 0.001)
+  values.forEach(v => { std.fit(v) })
+  _.true(std.value - 2.701851217 < 0.001)
   _.end()
 })
